e4ee72b782c3b67b0cf6c01e231c18e123fddff2
GabrielSoaresGomes <63868800+GabrielSoaresGomes@users.noreply.github.com>
2023-03-16 17:38:32 +0000

    fix: Removendo importação desnecessária

--- users/models.py
+++ users/models.py
@@ -1,6 +1,5 @@
 import datetime
 
-from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
 from django.contrib.auth.models import (
     BaseUserManager, AbstractBaseUser
