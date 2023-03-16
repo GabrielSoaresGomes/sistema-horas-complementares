61cb60f50cc860c6c450a0669b866974bdb3ce7d
GabrielSoaresGomes <63868800+GabrielSoaresGomes@users.noreply.github.com>
2023-03-16 14:07:55 +0000

    feat: Finalizando configuração inicial da estrutura

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# sistema-horas-complementares
+# Sistema de Horas Complementares
+
+Criação de um serviço que facilite o registro e o monitoramento de horas complementares
+
+## Regras de Projeto
+
+### String
+
+- Utilizar apóstrofo(aspas simples) para string no código
+- Utilizar aspas duplas para string em arquivo que envolva json ou nos .envs
+
+### Nome de Variáveis
+
+- Utilizar snake_case para variáveis comuns, métodos, propriedades de JSON e para nome de colunas do banco
+- Utilizar ChamelCase para nome de classes
+ 
--- project/settings.py
+++ project/settings.py
@@ -1,4 +1,5 @@
 import os
+import json
 from pathlib import Path
 from dotenv import load_dotenv
 load_dotenv()
@@ -6,20 +7,16 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = []
+csrf_trusted_origins_env = os.getenv('CSRF_TRUSTED_ORIGINS')
+CSRF_TRUSTED_ORIGINS = json.loads(csrf_trusted_origins_env)
 
+allowed_hosts_env = os.getenv('ALLOWED_HOSTS')
+ALLOWED_HOSTS = json.loads(allowed_hosts_env)
 
-# Application definition
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -93,9 +90,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/4.1/topics/i18n/
-
 LANGUAGE_CODE = "pt-br"
 
 TIME_ZONE = 'America/Sao_Paulo'
@@ -110,9 +104,5 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-ALLOWED_HOSTS=["*"]
