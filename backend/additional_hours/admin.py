6c54a2ffaad2e344923e620497e555fee0f88e0d
Junior <junior.ng@outlook.com.br>
2023-03-31 19:34:44 -0300

    feat: permite adicionar todos os models pelo admin

--- backend/additional_hours/admin.py
+++ backend/additional_hours/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Activity, Course, ActivityCourse, UserActivity, UserCourse
+
+class ActivityAdmin(admin.ModelAdmin):
+    list_display = ('__all__')
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('__all__')
+
+class ActivityCourseAdmin(admin.ModelAdmin):
+    list_display = ('__all__')
+
+class UserActivityAdmin(admin.ModelAdmin):
+    list_display = ('__all__')
+
+class UserCourseAdmin(admin.ModelAdmin):
+    list_display = ('__all__')
 
 # Register your models here.
+admin.site.register(Activity, ActivityAdmin)
+admin.site.register(Course, CourseAdmin)
+admin.site.register(ActivityCourse, ActivityCourseAdmin)
+admin.site.register(UserActivity, UserActivityAdmin)
+admin.site.register(UserCourse, UserCourseAdmin)
