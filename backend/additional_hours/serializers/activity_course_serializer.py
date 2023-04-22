a7f64302fd66a0fc417509049efd665798ac6075
Gabriel Dragon <gabrielsoares221@gmail.com>
2023-04-22 14:35:32 -0300

    fix: Correção no serializer do ActivityCourse

--- backend/additional_hours/serializers/activity_course_serializer.py
+++ backend/additional_hours/serializers/activity_course_serializer.py
@@ -1,10 +1,14 @@
 from django.db.models import fields
 from rest_framework import serializers
 
-from additional_hours.models import ActivityCourse
+from additional_hours.models import ActivityCourse, Activity, Course
+from additional_hours.serializers import ActivitySerializer, CourseSerializer
 
 
-class CourseSerializer(serializers.ModelSerializer):
+class ActivityCourseSerializer(serializers.ModelSerializer):
+    course_id = serializers.IntegerField(allow_null=True, required=False)
+    activity_id = serializers.IntegerField(allow_null=True, required=False)
+
     class Meta:
         model = ActivityCourse
-        fields = ('id', 'course', 'activity', 'maximum_hours', 'created_at', 'deleted_at')
+        fields = ('id', 'activity_id', 'course_id', 'maximum_hours', 'created_at', 'deleted_at')
