f60189942411ebb6e7e1e7c7460de55f59c676ba
Gabriel Dragon <gabrielsoares221@gmail.com>
2023-05-07 13:03:55 -0300

    feat: Adicionando environment_validation e mudando env de local

---
+++ backend/environment_validation.py
@@ -0,0 +1,49 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class EnvironmentValidation:
+    def __init__(self):
+        self.env = None
+        self.get_env_data()
+
+    def get_env_data(self):
+        try:
+            self.env = {
+                'DJANGO_DEBUG': os.getenv('DJANGO_DEBUG'),
+                'SECRET_KEY': os.getenv('SECRET_KEY'),
+                'CSRF_TRUSTED_ORIGINS': os.getenv('CSRF_TRUSTED_ORIGINS'),
+                'ALLOWED_HOSTS': os.getenv('ALLOWED_HOSTS'),
+                'ADMIN_USER': os.getenv('ADMIN_USER'),
+                'ADMIN_PASSWORD': os.getenv('ADMIN_PASSWORD'),
+                'DATABASE_POSTGRES_NAME': os.getenv('DATABASE_POSTGRES_NAME'),
+                'DATABASE_POSTGRES_USER': os.getenv('DATABASE_POSTGRES_USER'),
+                'DATABASE_POSTGRES_PASSWORD': os.getenv('DATABASE_POSTGRES_PASSWORD'),
+                'DATABASE_POSTGRES_HOST': os.getenv('DATABASE_POSTGRES_HOST'),
+                'DATABASE_POSTGRES_PORT': os.getenv('DATABASE_POSTGRES_PORT'),
+                'IS_USING_SQLITE3': os.getenv('IS_USING_SQLITE3'),
+                'DRIVE_EMAIL': os.getenv('DRIVE_EMAIL'),
+                'DRIVE_PASSWORD': os.getenv('DRIVE_PASSWORD'),
+                'DRIVE_PROJECT_NAME': os.getenv('DRIVE_PROJECT_NAME'),
+                'GS_BUCKET_NAME': os.getenv('GS_BUCKET_NAME'),
+                'DEFAULT_FILE_STORAGE': os.getenv('DEFAULT_FILE_STORAGE')
+            }
+            errors_list = list()
+            for data in self.env:
+                if not self.env.get(data):
+                    errors_list.append(data)
+            if len(errors_list):
+                print(f'Os seguintes dados estão inválidos no .env: {errors_list}!')
+            else:
+                print('Todos dados no .env estão certo!')
+        except Exception as error:
+            print(f"Erro ao pegar os dados da env: {error}")
+
+    def get(self, env_key):
+        env_data = self.env.get(env_key)
+        if env_data:
+            return env_data
+        else:
+            return None
--- tests/http/verify_request.py
+++ tests/http/verify_request.py
@@ -74,61 +74,64 @@ urlsDict = {
 
 
 def verifyActivityRequest():
-    print(f'{blueText}--- INICIANDO VERIFICAÇÃO ---{resetColor}')
+    try:
+        print(f'{blueText}--- INICIANDO VERIFICAÇÃO ---{resetColor}')
 
-    # Iniciando a contagem de tempo e definindo variáveis de lista de erros
-    start_time = time.time()
-    errorList500 = []
-    errorList400 = []
-    errorList300 = []
+        # Iniciando a contagem de tempo e definindo variáveis de lista de erros
+        start_time = time.time()
+        errorList500 = []
+        errorList400 = []
+        errorList300 = []
 
-    # Loop fazendo as requisições e validações
-    for fullUrl in urlsDict:
+        # Loop fazendo as requisições e validações
+        for fullUrl in urlsDict:
 
-        # Formatando os valores do objeto em variáveis mais simples
-        method = fullUrl.split('|')[0]
-        url = fullUrl.split('|')[1]
-        body = urlsDict.get(fullUrl)
-        response = ''
+            # Formatando os valores do objeto em variáveis mais simples
+            method = fullUrl.split('|')[0]
+            url = fullUrl.split('|')[1]
+            body = urlsDict.get(fullUrl)
+            response = ''
 
-        # Verificação de qual método será utilizado na requisição
-        if method == 'GET':
-            response = requests.get(url)
-        elif method == 'POST':
-            response = requests.post(url, json=body)
-        elif method == 'PUT':
-            response = requests.put(url, json=body)
-        elif method == 'DELETE':
-            response = requests.delete(url)
-        else:
-            print(f'{redText}ALGO ESTÁ ERRADO!${resetColor}')
+            # Verificação de qual método será utilizado na requisição
+            if method == 'GET':
+                response = requests.get(url)
+            elif method == 'POST':
+                response = requests.post(url, json=body)
+            elif method == 'PUT':
+                response = requests.put(url, json=body)
+            elif method == 'DELETE':
+                response = requests.delete(url)
+            else:
+                print(f'{redText}ALGO ESTÁ ERRADO!${resetColor}')
 
-        # Pegando o resultado da requisição
-        status_code = response.status_code
-        responseDict = {
-            fullUrl: response.text
-        }
+            # Pegando o resultado da requisição
+            status_code = response.status_code
+            responseDict = {
+                fullUrl: response.text
+            }
 
-        # Verificando se houve erros na requisição e inserindo na lista de erros
-        errorList500.append(responseDict) if 600 > status_code >= 500 else ''
-        errorList400.append(responseDict) if 500 > status_code >= 400 else ''
-        errorList300.append(responseDict) if 400 > status_code >= 300 else ''
+            # Verificando se houve erros na requisição e inserindo na lista de erros
+            errorList500.append(responseDict) if 600 > status_code >= 500 else ''
+            errorList400.append(responseDict) if 500 > status_code >= 400 else ''
+            errorList300.append(responseDict) if 400 > status_code >= 300 else ''
 
-    # Exibindo os erros que foram encontrados
-    print(f'{redText}ERROS 500+:{resetColor}')
-    print(f'{redText}{errorList500}{resetColor}')
-    print(f'{magentaText}ERROS 400+:{resetColor}')
-    print(f'{magentaText}{errorList400}{resetColor}')
-    print(f'{yellowText}ERROS 300+:{resetColor}')
-    print(f'{yellowText}{errorList300}{resetColor}')
-    print(f'{blueText}--- FIM DA VERIFICAÇÃO ---{resetColor}')
+        # Exibindo os erros que foram encontrados
+        print(f'{redText}ERROS 500+:{resetColor}')
+        print(f'{redText}{errorList500}{resetColor}')
+        print(f'{magentaText}ERROS 400+:{resetColor}')
+        print(f'{magentaText}{errorList400}{resetColor}')
+        print(f'{yellowText}ERROS 300+:{resetColor}')
+        print(f'{yellowText}{errorList300}{resetColor}')
+        print(f'{blueText}--- FIM DA VERIFICAÇÃO ---{resetColor}')
 
-    # Finalizando a contagem do método e arredondando o valor para 2 casas decimais
-    end_time = time.time()
-    total_time = round(end_time - start_time, 2)
+        # Finalizando a contagem do método e arredondando o valor para 2 casas decimais
+        end_time = time.time()
+        total_time = round(end_time - start_time, 2)
 
-    # Exibindo o tempo total gasto no método
-    print(f'{cyanText}Tempo de execução: {total_time} segundos!{resetColor}')
+        # Exibindo o tempo total gasto no método
+        print(f'{cyanText}Tempo de execução: {total_time} segundos!{resetColor}')
+    except Exception as error:
+        print(f'Erro ao fazer testes, verifique se o back está rodando: {error}')
 
 
 verifyActivityRequest()
