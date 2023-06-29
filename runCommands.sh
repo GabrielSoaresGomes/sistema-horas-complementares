1cb8c92f33dce4d48a844b5ae97cc37d2cd34c22
Gabriel Soares <gabriel.soares@smartnx.com>
2023-06-29 11:06:33 -0300

    Alterando arquivo runCommands e removendo arquivo runMigration

--- runCommands.sh
+++ runCommands.sh
@@ -1,3 +1,8 @@
+AZUL='\033[0;34m'
+VERMELHO='\033[31m'
+CIANO='\033[36m'
+NC='\033[0m'
+
 # Executar enquanto o projeto estiver rodando
 declare -A commands_map=(
     ["l"]="logs"
@@ -22,8 +27,17 @@ declare -A commands_map=(
     ["stop-p"]="stop-postgres"
 
     ["fill-database"]="fill-database"
+
+    ["m"]="migrate"
+    ["migrate"]="migrate"
+
+    ["makemigrations"]="makemigrations"
+    ["mm"]="makemigrations"
 )
-if [[ ! -z "$1" ]]; then
+
+ordered_keys=("l" "log" "logs" "|" "t" "terminal" "|" "s" "start" "|" "stop" "|" "start-postgres" "s-postgres" "start-p" "s-p" "sp" "|" "stop-postgres" "stop-p" "|" "fill-database" "|" "m" "migrate" "|" "makemigrations" "mm")
+
+if [ -v commands_map["$1"] ]; then
     command=${commands_map[$1]}
     if [ "$command" == "logs" ]; then
         docker logs --follow sistema-horas-complementares-web-1
@@ -39,10 +53,19 @@ if [[ ! -z "$1" ]]; then
         docker start postgres-horas-complementares
     elif [ "$command" = "fill-database" ]; then
         docker exec -it postgres-horas-complementares psql -h localhost -U postgres horas_complementares -a -p 5432 -f fill_database.sql
+    elif [ "$command" = "migrate" ]; then
+        docker-compose exec web python backend/manage.py migrate
+    elif [ "$command" = "makemigrations" ]; then
+        docker-compose exec web python backend/manage.py makemigrations
     fi
 else
-   for i in "${!commands_map[@]}"
-        do
-            echo "${i}=${commands_map[$i]}"
-        done
+    echo -e "$VERMELHO""Comando não encontrado, utilize alguns dos comandos abaixo: $NC\n"
+    for key in "${ordered_keys[@]}"; do
+        if [ "$key" = "|" ]; then
+            echo ""
+        else
+            value="${commands_map[$key]}"
+            echo -e "$AZUL$key$NC = $CIANO$value$NC"
+        fi
+    done
 fi
--- runMigration.sh
+++
@@ -1,8 +0,0 @@
-# Executar enquanto o projeto estiver rodando
-if [ "$1" == "migrate" ];then
-    docker-compose exec web python backend/manage.py migrate
-elif [ "$1" == "makemigrations" ];then
-    docker-compose exec web python backend/manage.py makemigrations
-else
-    echo "Argumento inválido, insira com um dos 2 argumentos à seguir: [ migrate | makemigrations ]"
-fi
