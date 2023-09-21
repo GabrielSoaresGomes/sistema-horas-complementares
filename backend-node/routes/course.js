b633283bd09e6bc4e4f928c65f049651cb425d64
Gabriel Soares <gabriel.soares@smartnx.com>
2023-09-21 11:59:40 -0300

    refact: Criando entidade para o apply-result

--- backend-node/routes/course.js
+++ backend-node/routes/course.js
@@ -3,25 +3,9 @@ const express = require('express');
 const CourseComponent = require('../components/course/course');
 const CourseRepository = require('../components/course/data/course-repository');
 const responseStatusCode = require('../entity/response-status-code');
+const applyResult = require('../entity/apply-result');
 
 const router = express.Router();
-
-const applyResult = (result, res, successStatusCode) => {
-    if (result.hasError()) {
-        if (result.hasCriticalError()) {
-            res.status(responseStatusCode.INTERNAL_SERVER_ERROR);
-        } else {
-            res.status(responseStatusCode.BAD_REQUEST);
-        }
-        res.send(result.getErrorList());
-    } else if (result.isResultEmpty()) {
-        res.status(responseStatusCode.BAD_REQUEST);
-        res.send([]);
-    } else {
-        res.status(successStatusCode);
-        res.send(result.getResult());
-    }
-};
 
 router.get('/', async (req, res) => {
     const couseComponent = new CourseComponent(new CourseRepository());
--- backend-node/routes/user.js
+++ backend-node/routes/user.js
@@ -3,25 +3,9 @@ const express = require('express');
 const UserComponent = require('../components/user/user');
 const UserRepository = require('../components/user/data/user-repository');
 const responseStatusCode = require('../entity/response-status-code');
+const applyResult = require('../entity/apply-result');
 
 const router = express.Router();
-
-const applyResult = (result, res, successStatusCode) => {
-    if (result.hasError()) {
-        if (result.hasCriticalError()) {
-            res.status(responseStatusCode.INTERNAL_SERVER_ERROR);
-        } else {
-            res.status(responseStatusCode.BAD_REQUEST);
-        }
-        res.send(result.getErrorList());
-    } else if (result.isResultEmpty()) {
-        res.status(responseStatusCode.BAD_REQUEST);
-        res.send([]);
-    } else {
-        res.status(successStatusCode);
-        res.send(result.getResult());
-    }
-};
 
 router.get('/', async (req, res) => {
     const userComponent = new UserComponent(new UserRepository());
